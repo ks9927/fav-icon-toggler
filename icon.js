d67c3a50fc9d7b33c9fe5635fbe892eb57fb5d2b
KS <180429671+ks9927@users.noreply.github.com>
2025-05-09 19:14:12 -0400

    added toggleBtn function

--- icon.js
+++ icon.js
@@ -1,7 +1,16 @@
 const heartBtn = document.querySelectorAll("button");
 
+function toggleBtn(btn) {
+  btn.classList.toggle("filled");
+  if(btn.classList.contains("filled")) {
+    btn.innerHTML = "&#10084;";
+  }
+  else {
+  btn.innerHTML = "&#9825;";
+  }
+}
 heartBtn.forEach(btn => {
   btn.addEventListener("click", () => {
-    btn.classList.toggle("filled");
+   toggleBtn(btn);
   })
 })
